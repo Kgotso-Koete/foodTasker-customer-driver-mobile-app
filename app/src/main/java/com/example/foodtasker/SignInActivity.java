7e157241b18c503b64381035d00a71203e68cfca
“Kgotso-Koete” <kgkoete@gmail.com>
2020-01-15 00:18:45 +0200

    Testing out Android studio commit

--- app/src/main/java/com/example/foodtasker/SignInActivity.java
+++ app/src/main/java/com/example/foodtasker/SignInActivity.java
@@ -3,7 +3,7 @@ package com.example.foodtasker;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
