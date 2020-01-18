f277cc3d28692020b55746f62ba1da67feb622aa
“Kgotso-Koete” <kgkoete@gmail.com>
2020-01-19 01:41:18 +0200

    Add Driver Screen

---
+++ app/src/main/java/com/example/foodtasker/DriverMainActivity.java
@@ -0,0 +1,101 @@
+package com.example.foodtasker;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+//import com.example.foodtasker.Utils.CircleTransform;
+//import com.example.foodtasker.Fragments.DeliveryFragment;
+//import com.example.foodtasker.Fragments.OrderListFragment;
+import com.example.foodtasker.R;
+//import com.example.foodtasker.Fragments.StatisticFragment;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DriverMainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.icon_menu_24dp);
+        // remove title from action bar
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        mDrawerLayout = findViewById(R.id.drawer_layout_driver);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        int id = menuItem.getItemId();
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        if (id == R.id.nav_orders) {
+                            //transaction.replace(R.id.content_frame, new RestaurantListFragment()).commit();
+                        } else if (id == R.id.nav_delivery) {
+                            //transaction.replace(R.id.content_frame, new TrayFragment()).commit();
+                        } else if (id == R.id.nav_statistic) {
+                            // transaction.replace(R.id.content_frame, new OrderFragment()).commit();
+                        } else if (id == R.id.nav_logout) {
+
+                        }
+
+                        return true;
+                    }
+                });
+
+        //FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //transaction.replace(R.id.content_frame, new RestaurantListFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/foodtasker/SignInActivity.java
+++ app/src/main/java/com/example/foodtasker/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.foodtasker;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
@@ -8,19 +9,55 @@ import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private Button customerButton, driverButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        customerButton = (Button) findViewById(R.id.button_customer);
+        driverButton = (Button) findViewById(R.id.button_driver);
+
+        // Handle customer button: switch colours when pressed
+        customerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customerButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                customerButton.setTextColor(getResources().getColor(android.R.color.white));
+
+                driverButton.setBackgroundColor(getResources().getColor(android.R.color.white));
+                driverButton.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
+
+        // Handle driver button: switch colours when pressed
+        driverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                driverButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                driverButton.setTextColor(getResources().getColor(android.R.color.white));
+
+                customerButton.setBackgroundColor(getResources().getColor(android.R.color.white));
+                customerButton.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
+
+
+        // Handle log in
         Button buttonLogin = (Button) findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ColorDrawable customerbuttonColor = (ColorDrawable) customerButton.getBackground();
 
-                Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
-                startActivity(intent);
-
+                if (customerbuttonColor.getColor() == getResources().getColor(R.color.colorAccent)) {
+                    Intent intent = new Intent(getApplicationContext(), CustomerMainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DriverMainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
