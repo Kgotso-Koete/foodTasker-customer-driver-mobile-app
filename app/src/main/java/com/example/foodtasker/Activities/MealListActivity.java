d10b9f57eccd0e24fc20671d766094db0ef248a8
“Kgotso-Koete” <kgkoete@gmail.com>
2020-01-21 14:55:35 +0200

    Getting Menu Data for each meal

--- app/src/main/java/com/example/foodtasker/Activities/MealListActivity.java
+++ app/src/main/java/com/example/foodtasker/Activities/MealListActivity.java
@@ -3,18 +3,36 @@ package com.example.foodtasker.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
+import android.util.Log;
 import android.widget.ListView;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.AdapterView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 //import com.example.foodtasker.Adapters.MealAdapter;
 //import com.example.foodtasker.Objects.Meal;
+import com.example.foodtasker.BuildConfig;
 import com.example.foodtasker.R;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MealListActivity extends AppCompatActivity {
+
+    String LOCAL_API_URL = BuildConfig.LOCAL_API_URL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +42,7 @@ public class MealListActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String restaurantId = intent.getStringExtra("restaurantId");
         String restaurantName = intent.getStringExtra("restaurantName");
-        
+
         getSupportActionBar().setTitle(restaurantName);
 
         ListView listView = (ListView) findViewById(R.id.meal_list);
@@ -57,5 +75,34 @@ public class MealListActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Get Meals list
+        getMeals(restaurantId);
+    }
+
+    private void getMeals(String restaurantId) {
+        String url = LOCAL_API_URL + "/customer/meals/" + restaurantId + "/";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("MEAL LIST", response.toString());
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
     }
 }
--- app/src/main/java/com/example/foodtasker/Fragments/RestaurantListFragment.java
+++ app/src/main/java/com/example/foodtasker/Fragments/RestaurantListFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.BaseAdapter;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
