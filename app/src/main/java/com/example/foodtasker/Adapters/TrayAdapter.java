a576f07c6c4eca3a744b6573db5476e87b807566
“Kgotso-Koete” <kgkoete@gmail.com>
2020-01-28 01:23:43 +0200

    Showing Tray on the List

---
+++ app/src/main/java/com/example/foodtasker/Adapters/TrayAdapter.java
@@ -0,0 +1,58 @@
+package com.example.foodtasker.Adapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.foodtasker.Objects.Tray;
+import com.example.foodtasker.R;
+
+import java.util.ArrayList;
+
+public class TrayAdapter extends BaseAdapter {
+
+    private Activity activity;
+    private ArrayList<Tray> trayList;
+
+    public TrayAdapter(Activity activity, ArrayList<Tray> trayList) {
+        this.activity = activity;
+        this.trayList = trayList;
+    }
+
+    @Override
+    public int getCount() {
+        return trayList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return trayList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if (view == null) {
+            view = LayoutInflater.from(activity).inflate(R.layout.list_item_tray, null);
+        }
+
+        TextView mealName = (TextView) view.findViewById(R.id.tray_meal_name);
+        TextView mealQuantity = (TextView) view.findViewById(R.id.tray_meal_quantity);
+        TextView mealSubTotal = (TextView) view.findViewById(R.id.tray_meal_subtotal);
+
+        Tray tray = trayList.get(i);
+        mealName.setText(tray.getMealName());
+        mealQuantity.setText(tray.getMealQuantity() + "");
+        mealSubTotal.setText("$" + (tray.getMealPrice() * tray.getMealQuantity()));
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/foodtasker/Fragments/TrayFragment.java
+++ app/src/main/java/com/example/foodtasker/Fragments/TrayFragment.java
@@ -1,28 +1,55 @@
 package com.example.foodtasker.Fragments;
 
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.os.AsyncTask;
-import android.util.Log;
-import android.annotation.SuppressLint;
-import android.widget.Toast;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import com.example.foodtasker.Activities.PaymentActivity;
-//import com.example.foodtasker.Adapters.TrayAdapter;
+import com.example.foodtasker.Adapters.TrayAdapter;
 import com.example.foodtasker.AppDatabase;
 import com.example.foodtasker.Objects.Tray;
-import com.example.foodtasker.Activities.PaymentActivity;
 import com.example.foodtasker.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -31,6 +58,8 @@ import java.util.List;
 public class TrayFragment extends Fragment {
 
     private AppDatabase db;
+    private ArrayList<Tray> trayList;
+    private TrayAdapter adapter;
 
 
     public TrayFragment() {
@@ -53,28 +82,12 @@ public class TrayFragment extends Fragment {
         db = AppDatabase.getAppDatabase(getContext());
         listTray();
 
-        ListView restaurantListView = (ListView) getActivity().findViewById(R.id.tray_list);
-        restaurantListView.setAdapter(new BaseAdapter() {
-            @Override
-            public int getCount() {
-                return 4;
-            }
+        trayList = new ArrayList<Tray>();
+        adapter = new TrayAdapter(this.getActivity(), trayList);
 
-            @Override
-            public Object getItem(int position) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                return LayoutInflater.from(getActivity()).inflate(R.layout.list_item_tray, null);
-            }
-        });
+        ListView listView = (ListView) getActivity().findViewById(R.id.tray_list);
+        listView.setAdapter(adapter);
+        
 
         Button buttonLogin = (Button) getActivity().findViewById(R.id.button_add_payment);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
@@ -99,8 +112,11 @@ public class TrayFragment extends Fragment {
             @Override
             protected void onPostExecute(List<Tray> trays) {
                 super.onPostExecute(trays);
-                for (Tray tray : trays) {
-                    Log.d("TRAY ITEM", tray.getMealName() + "-" + tray.getMealQuantity());
+                if (!trays.isEmpty()) {
+                    // Refresh our listview
+                    trayList.clear();
+                    trayList.addAll(trays);
+                    adapter.notifyDataSetChanged();
                 }
             }
         }.execute();
